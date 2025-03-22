c53a886a08ae50d8045e6e3ce6193db6d12bb3a9
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-03-22 14:03:34 +0200

    feat: add CoutryCard component

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+interface Country {
+  cca3: string;
+  flags: { png: string };
+  name: { common: string };
+  population: number;
+  region: string;
+}
+
+const CountryCard = React.memo(({ country }: { country: Country }) => {
+  const [visited, setVisited] = useState(() => {
+    const visitedCountries: string[] = JSON.parse(
+      localStorage.getItem('visitedCountries') || '[]'
+    );
+    return visitedCountries.includes(country.cca3);
+  });
+
+  const toggleVisited = () => {
+    const visitedCountries: string[] = JSON.parse(
+      localStorage.getItem('visitedCountries') || '[]'
+    );
+    if (visited) {
+      const updatedVisited = visitedCountries.filter(
+        (code: string) => code !== country.cca3
+      );
+      localStorage.setItem('visitedCountries', JSON.stringify(updatedVisited));
+    } else {
+      visitedCountries.push(country.cca3);
+      localStorage.setItem(
+        'visitedCountries',
+        JSON.stringify(visitedCountries)
+      );
+    }
+    setVisited(!visited);
+  };
+
+  return (
+    <li>
+      <img src={country.flags.png} alt={`${country.name.common} flag`} />
+      <h3>{country.name.common}</h3>
+      <p>Population: {country.population}</p>
+      <p>Region: {country.region}</p>
+      <button onClick={toggleVisited}>
+        {visited ? 'Unmark as Visited' : 'Mark as Visited'}
+      </button>
+    </li>
+  );
+});
+
+CountryCard.displayName = 'CountryCard';
+
+export default CountryCard;
